c356a38b0d44dbf5e6249cbc4a9dfa9699e64951
Nicolas <nicolas.lecrique+git@globers.co>
2021-07-10 18:27:55 +0200

    Create Db connection config to connect to cloud sql
    cf.
    
    // cf. https://github.com/GoogleCloudPlatform/java-docs-samples/blob/master/cloud-sql/r2dbc/src/main/java/com/example/cloudsql/r2dbcsample/R2dbcSampleApplication.java
    // and https://github.com/GoogleCloudPlatform/spring-cloud-gcp/issues/205
    // and https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory/blob/main/r2dbc/postgres/src/test/java/com/google/cloud/sql/core/R2dbcPostgresIntegrationTests.java
    
    It is needed on top of using dependency com.google.cloud.sql:cloud-sql-connector-r2dbc-postgres
    default spring config doesn't create the correct connection string (with "gcp" in it)
    to be consistant we use this system also in dev mode

---
+++ src/main/kotlin/com/datatoggle/server/db/R2dbcConfig.kt
@@ -0,0 +1,42 @@
+package com.datatoggle.server.db
+
+import com.google.cloud.sql.core.GcpConnectionFactoryProvider.ENABLE_IAM_AUTH
+import io.r2dbc.pool.ConnectionPool
+import io.r2dbc.pool.ConnectionPoolConfiguration
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryOptions
+import io.r2dbc.spi.ConnectionFactoryOptions.DATABASE
+import io.r2dbc.spi.ConnectionFactoryOptions.DRIVER
+import io.r2dbc.spi.ConnectionFactoryOptions.HOST
+import io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD
+import io.r2dbc.spi.ConnectionFactoryOptions.PROTOCOL
+import io.r2dbc.spi.ConnectionFactoryOptions.USER
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+
+// cf. https://github.com/GoogleCloudPlatform/java-docs-samples/blob/master/cloud-sql/r2dbc/src/main/java/com/example/cloudsql/r2dbcsample/R2dbcSampleApplication.java
+// and https://github.com/GoogleCloudPlatform/spring-cloud-gcp/issues/205
+// and https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory/blob/main/r2dbc/postgres/src/test/java/com/google/cloud/sql/core/R2dbcPostgresIntegrationTests.java
+
+@Configuration
+@EnableR2dbcRepositories
+class R2dbcConfig(
+    @Value("\${datatoggle.db_connection}") private val dbConnection: String,
+) : AbstractR2dbcConfiguration() {
+
+    @Bean
+    override fun connectionFactory(): ConnectionFactory {
+
+        val connectionFactory: ConnectionFactory = ConnectionFactories.get(dbConnection)
+
+        val configuration = ConnectionPoolConfiguration
+            .builder(connectionFactory)
+            .build()
+
+        return ConnectionPool(configuration)
+    }
+}
