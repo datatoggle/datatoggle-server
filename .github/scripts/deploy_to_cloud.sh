496640050885f9733fd4e391b641f22c5a143956
Nicolas <nicolas.lecrique+git@globers.co>
2022-05-19 17:26:51 +0200

    try to deploy to cloud run
    reusing existing script for deployment, I just removed the jdk 11 ref because it's in setup-java action

---
+++ .github/scripts/deploy_to_cloud.sh
@@ -0,0 +1,10 @@
+# build jar
+# TODO: could be replaced with 2-step docker build (cf. cloud run quick start for java/spring)
+./gradlew build
+
+# build docker image then upload it to google container registry
+docker build . --tag gcr.io/datatoggle-b83b6/server
+docker push gcr.io/datatoggle-b83b6/server
+
+# deploy image from container registry to google cloud run
+gcloud run deploy server --image gcr.io/datatoggle-b83b6/server --platform managed --memory 1G --allow-unauthenticated --max-instances 1
