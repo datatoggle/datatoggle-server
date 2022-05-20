7c069cb60a2f0c684b17579dd854b355df953d52
Nicolas <nicolas.lecrique+git@globers.co>
2022-05-20 15:49:31 +0200

    Deploy to cloud using artifact registry
    previous usage with language package (with gradle) could not be pushed to cloud run, it needs docker container

--- deploy_to_cloud.sh
+++ deploy_to_cloud.sh
@@ -1,12 +1,17 @@
 export JAVA_HOME=$(/usr/libexec/java_home -v 11)
 
 # build jar
-# TODO: could be replaced with 2-step docker build (cf. cloud run quick start for java/spring)
 ./gradlew build
 
+
 # build docker image then upload it to google container registry
-docker build . --tag gcr.io/datatoggle-b83b6/server
-docker push gcr.io/datatoggle-b83b6/server
+docker build . --tag europe-west1-docker.pkg.dev/datatoggle-b83b6/containers-for-cloud-run-repo/server
+
+# Nb: it requires docker to know about this registry, you must do the following command before
+gcloud auth configure-docker europe-west1-docker.pkg.dev --quiet
+docker push europe-west1-docker.pkg.dev/datatoggle-b83b6/containers-for-cloud-run-repo/server
 
 # deploy image from container registry to google cloud run
-gcloud run deploy server --image gcr.io/datatoggle-b83b6/server --platform managed --memory 1G --allow-unauthenticated --max-instances 1
+gcloud run deploy server --image europe-west1-docker.pkg.dev/datatoggle-b83b6/containers-for-cloud-run-repo/server --platform managed --memory 1G --allow-unauthenticated --max-instances 1
+
+
